47bbe2dd02ea1f0bb0ae44cd0a9869501f1dfcf8
Stéphane Martarello <stephane.martarello@crossknowledge.com>
2016-04-10 22:20:53 +0200

    Added license information in the source files

--- src/UI/Explorer/Tabs/Table/TableModel.cpp
+++ src/UI/Explorer/Tabs/Table/TableModel.cpp
@@ -1,9 +1,19 @@
-/*
- * TableDataTabModel.cpp
+/**
+ * Copyright (C) 2016  Stéphane Martarello
  *
- *  Created on: Mar 4, 2016
- *      Author: stephane
- */
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
 
 #include <UI/Explorer/Tabs/Table/TableModel.h>
 #include <QSqlQuery>
